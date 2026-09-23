95a7810db3fa524e90e0717d5f54fca98d57ae3d
Luís Miguel <lm.luismiguel10@gmail.com>
2026-09-23 15:41:11 -0300

    Clean up docblocks in Vitalista.php
    Removed unnecessary docblocks for getter and setter methods.

--- modelo/Vitalista.php
+++ modelo/Vitalista.php
@@ -7,17 +7,11 @@ class Vitalista extends Suporte {
     protected int $danoDivino;
     protected int $regeneracao;
 
-    /**
-     * Get the value of danoDivino
-     */
     public function getDanoDivino(): int
     {
         return $this->danoDivino;
     }
 
-    /**
-     * Set the value of danoDivino
-     */
     public function setDanoDivino(int $danoDivino): self
     {
         $this->danoDivino = $danoDivino;
@@ -25,17 +19,11 @@ class Vitalista extends Suporte {
         return $this;
     }
 
-    /**
-     * Get the value of regeneracao
-     */
     public function getRegeneracao(): int
     {
         return $this->regeneracao;
     }
 
-    /**
-     * Set the value of regeneracao
-     */
     public function setRegeneracao(int $regeneracao): self
     {
         $this->regeneracao = $regeneracao;
